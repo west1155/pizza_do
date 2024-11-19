8a26a58d31ff70d165c00398ba59aa5b92c64150
west1155 <westsupport@gmail.com>
2024-11-19 19:18:59 +0000

    tables created

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -21,6 +21,67 @@ async function up() {
         ]
 
     })
+
+    await prisma.product.createMany({
+        data: [{
+            name: 'Sandwich with bacon and cheese',
+            imageUrl: './public/images/11EED646B7AC9C38BA256320DD31C4D5.avif',
+            categoryId: 3,
+        },
+            {
+                name: 'Chicken nuggets',
+                imageUrl:
+                    './public/images/11EE7D61B9521D369D61228456C8F6C9.avif',
+                categoryId: 3,
+            },
+            {
+                name: 'Baked potatoes with sauce',
+                imageUrl: './public/images/production/11EED646B7AC9C38BA256320DD31C4D5.avif',
+                categoryId: 3,
+            }]
+    })
+
+    await prisma.ingredient.createMany({
+        data: [
+            {
+                name: 'Bacon',
+                price: 1.5,
+                imageUrl: './public/images/ingridients/bacon.png',
+            },
+            {
+                name: 'Cheese',
+                price: 0.5,
+                imageUrl: './public/images/ingridients/mozarella.png',
+            },
+            {
+                name: 'Mushrooms',
+                price: 1.0,
+                imageUrl: './public/images/ingridients/mushrooms.png',
+            },
+            {
+                name: 'Cholopens',
+                price: 1.0,
+                imageUrl: './public/images/ingridients/cholopens.png',
+            },
+        ],
+    })
+
+    await prisma.category.createMany({
+        data: [
+            {
+                name: 'Pizza',
+            },
+            {
+                name: 'Burgers',
+            },
+            {
+                name: 'Snacks',
+            },
+            {
+                name: 'Drinks',
+            },
+        ],
+    })
 }
 
 async function down() {
